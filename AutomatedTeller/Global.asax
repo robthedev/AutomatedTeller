8105819d2e2a8d6e8028af15417e6f5c11e9d64d
585media <585media@gmail.com>
2017-07-11 13:51:49 -0500

    Add project files.

---
+++ AutomatedTeller/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutomatedTeller.MvcApplication" Language="C#" %>
---
+++ AutomatedTeller/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutomatedTeller.Startup))]
+namespace AutomatedTeller
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
